57fb1c4329b7448983d9cf5cc45a40b84dddb638
James Hurley <jamesghurley@gmail.com>
2012-06-15 13:14:22 -0700

    add some preview stuff

---
+++ Face Hunter/Face Hunter/ERTGLCameraOutputProtocol.h
@@ -0,0 +1,13 @@
+//
+//  ERTGLCameraOutputProtocol.h
+//  Face Hunter
+//
+//  Created by James Hurley on 6/15/12.
+//  Copyright (c) 2012 James Hurley. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol ERTGLCameraOutputProtocol <NSObject>
+
+@end
